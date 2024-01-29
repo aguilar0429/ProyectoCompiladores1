7288296b3a787efd30bd827510e1f416bbf9925e
Eduardo Aguilar <eaguilaortiz0403@gmail.com>
2024-01-29 15:14:43 -0600

    prueba

--- ProyectoCompi/src/Main.java
+++ ProyectoCompi/src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("HI");
     }
 }

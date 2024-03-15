bf72619d9669e90f167ea81210c7816ec813c5ef
Eduardo Aguilar <eaguilaortiz0403@gmail.com>
2024-03-15 15:22:51 -0600

    bug fix :)

--- ProyectoCompi/src/Main.java
+++ ProyectoCompi/src/Main.java
@@ -6,19 +6,11 @@ import java.io.File;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.BaseErrorListener;
-
-import org.antlr.v4.runtime.tree.*;
-import java.util.Collections;
-import java.util.List;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 import static org.antlr.v4.runtime.CharStreams.fromFileName;
-import org.antlr.v4.runtime.tree.*;
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Collections;
+import org.antlr.v4.runtime.tree.ParseTree;
 public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
     static ArrayList<ErrorManagement> ListaErrores= new ArrayList<>();
     public static void main(String[] args) throws Exception{
 
@@ -40,13 +32,9 @@ public class Main {
         CharStream ch = fromFileName(input);
 
         MiniPascalLexer lexer = new MiniPascalLexer(ch);
-        MiniPascalLexer lexer2 = new MiniPascalLexer(ch);
-        CommonTokenStream tokens2 = new CommonTokenStream(lexer2);
-        MiniPascalParser parser2 = new MiniPascalParser(tokens2);
-        String str = new MiniPascalNewVisitor().visit(parser2.program());
-        System.out.println(str);
-
-
+       // MiniPascalLexer lexer2 = new MiniPascalLexer(ch);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MiniPascalParser parser = new MiniPascalParser(tokens);
         lexer.removeErrorListeners();
 
         lexer.addErrorListener(new BaseErrorListener() {
@@ -70,10 +58,9 @@ public class Main {
             }
 
         });
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        MiniPascalParser parser = new MiniPascalParser(tokens);
 
         parser.removeErrorListeners();
+
         parser.addErrorListener(new BaseErrorListener() {
             @Override
             public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
@@ -90,28 +77,29 @@ public class Main {
 
                 System.out.println(error_color + "Error Sintactico: " + line + ":" + charPositionInLine + " " + msgTranslated);
                 underlineError(recognizer,(Token)offendingSymbol, line, charPositionInLine);
-
-
-
             }
-        public  void underlineError(Recognizer recognizer,
-                Token offendingToken, int line,
-        int charPositionInLine) {
-            CommonTokenStream tokens = (CommonTokenStream)recognizer.getInputStream();
-            String input = tokens.getTokenSource().getInputStream().toString();
-            String[] lines = input.split("\n");
-            String errorLine = lines[line - 1];
-            System.err.println(errorLine);
-            for (int i=0; i<charPositionInLine; i++) System.err.print(" ");
-            int start = offendingToken.getStartIndex();
-            int stop = offendingToken.getStopIndex();
-            if ( start>=0 && stop>=0 ) {
-                for (int i=start; i<=stop; i++) System.err.print("^");
+            public  void underlineError(Recognizer recognizer,
+                                        Token offendingToken, int line,
+                                        int charPositionInLine) {
+                CommonTokenStream tokens = (CommonTokenStream)recognizer.getInputStream();
+                String input = tokens.getTokenSource().getInputStream().toString();
+                String[] lines = input.split("\n");
+                String errorLine = lines[line - 1];
+                System.err.println(errorLine);
+                for (int i=0; i<charPositionInLine; i++) System.err.print(" ");
+                int start = offendingToken.getStartIndex();
+                int stop = offendingToken.getStopIndex();
+                if ( start>=0 && stop>=0 ) {
+                    for (int i=start; i<=stop; i++) System.err.print("^");
+                }
+                System.err.println();
             }
-            System.err.println();
-        }
         });
-        parser.program();
+
+        ParseTree arbolito = parser.program();
+        MiniPascalNewVisitor visitante = new MiniPascalNewVisitor();
+        String str = visitante.visit(arbolito);
+        System.out.println(ANSI_RESET + str  );
     }
 
 }

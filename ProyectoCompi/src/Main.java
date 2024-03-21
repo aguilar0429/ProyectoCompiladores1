79a0f8593c18e6d9a2c81b05fc99736f781f96a3
Josue Espinal <josueespinalsanz@gmail.com>
2024-03-20 19:53:35 -0600

    fix

--- ProyectoCompi/src/Main.java
+++ ProyectoCompi/src/Main.java
@@ -78,22 +78,22 @@ public class Main {
                 System.out.println(error_color + "Error Sintactico: " + line + ":" + charPositionInLine + " " + msgTranslated);
                 //underlineError(recognizer,(Token)offendingSymbol, line, charPositionInLine);
             }
-            public  void underlineError(Recognizer recognizer,
-                                        Token offendingToken, int line,
-                                        int charPositionInLine) {
-                CommonTokenStream tokens = (CommonTokenStream)recognizer.getInputStream();
-                String input = tokens.getTokenSource().getInputStream().toString();
-                String[] lines = input.split("\n");
-                String errorLine = lines[line - 1];
-                System.err.println(errorLine);
-                for (int i=0; i<charPositionInLine; i++) System.err.print(" ");
-                int start = offendingToken.getStartIndex();
-                int stop = offendingToken.getStopIndex();
-                if ( start>=0 && stop>=0 ) {
-                    for (int i=start; i<=stop; i++) System.err.print("^");
-                }
-                System.err.println();
-            }
+//            public  void underlineError(Recognizer recognizer,
+//                                        Token offendingToken, int line,
+//                                        int charPositionInLine) {
+//                CommonTokenStream tokens = (CommonTokenStream)recognizer.getInputStream();
+//                String input = tokens.getTokenSource().getInputStream().toString();
+//                String[] lines = input.split("\n");
+//                String errorLine = lines[line - 1];
+//                System.err.println(errorLine);
+//                for (int i=0; i<charPositionInLine; i++) System.err.print(" ");
+//                int start = offendingToken.getStartIndex();
+//                int stop = offendingToken.getStopIndex();
+//                if ( start>=0 && stop>=0 ) {
+//                    for (int i=start; i<=stop; i++) System.err.print("^");
+//                }
+//                System.err.println();
+//            }
         });
 
         ParseTree arbolito = parser.program();
